41f464e4f453deeae3cafc8c66a3ce1f3be055ab
Mehardeep Singh Arneja <89298509+Mehardeep-Singh-Arneja@users.noreply.github.com>
2025-11-21 00:45:47 +0530

    Refactor matrix operations to use typedef for matrices

--- c_projects/matrix_operations.c
+++ c_projects/matrix_operations.c
@@ -1,98 +1,132 @@
 #include <stdio.h>
 
-void print(int mat1[3][3],int n){
-    for (int i = 0; i<n;i++ ){
-        for (int z= 0; z<n;z++ ){
-        printf("%d ",mat1[i][z]);
+// find cayley hamilton eq , inverse of matrix using cayley hamilton eq, determinant, sum , difference, product of matrices.
+// you can also print matrices using print() function neatly.
+
+typedef float mat[3][3];
+
+mat identity = {
+    {1,0,0},
+    {0,1,0},
+    {0,0,1}
+};
+
+void print(mat m, int n){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            printf("%0.2f ", m[i][j]);
         }
         printf("\n");
     }
     printf("\n");
 }
 
-void sum(int mat1[3][3],int mat2[3][3],int n,int result[3][3]){ // sum of 2 matrices
-    for (int i = 0; i< n;i++){
-        for (int y = 0; y< n;y++){
-            result[i][y] = mat1[i][y] + mat2[i][y];
+void sum(mat A, mat B, int n, mat R){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            R[i][j] = A[i][j] + B[i][j];
         }
     }
 }
 
-void mult(int mat1[3][3],int mat2[3][3],int n,int result[3][3]){ // multiplication of 2 matrices
-    int c = 0;
-    
-    for (int i = 0; i< n;i++){
-        for (int y = 0; y< n;y++){
-            c = 0;
-            for (int j = 0;j<n;j++){
-               c += mat1[i][j]* mat2[j][y];
+void diff(mat A, mat B, int n, mat R){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            R[i][j] = A[i][j] - B[i][j];
+        }
+    }
+}
+
+void scalar(mat A, float s, int n, mat R){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            R[i][j] = A[i][j] * s;
+        }
+    }
+}
+
+void mult(mat A, mat B, int n, mat R){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            float c = 0;
+            for (int k = 0; k < n; k++){
+                c += A[i][k] * B[k][j];
             }
-            result[i][y] = c;
+            R[i][j] = c;
         }
     }
 }
 
-int det(int mat1[3][3]) // determinent of matrix
+float det(mat A)
 {
-    int c1 = 0,h = 1;
-    int min_idx[3][2] = {
-        {1,2},
-        {0,2},
-        {0,1},
-    };
-    for (int i = 0;i < 3;i++){
-        h = (i == 1) ? -1 : 1;
-        c1 += h*mat1[0][i]*((mat1[1][min_idx[i][0]]*mat1[2][min_idx[i][1]])-(mat1[1][min_idx[i][1]]*mat1[2][min_idx[i][0]]));
-    }
-    return c1;
+    float d = 0;
+    d += A[0][0] * (A[1][1]*A[2][2] - A[1][2]*A[2][1]);
+    d -= A[0][1] * (A[1][0]*A[2][2] - A[1][2]*A[2][0]);
+    d += A[0][2] * (A[1][0]*A[2][1] - A[1][1]*A[2][0]);
+    return d;
 }
 
-void cayley_hamilton(int mat2[3][3]){ // cayley hamilton equation of any 3x3 matrix
-    int x =0;
-    int det1 = det(mat2);
-    int i = 0;
-    for (i = 0; i < 3;i++){
-        x += mat2[i][i];
-    }
+void inverse(mat A, float eq[3], int n, mat R){
+    mat A2, xA, qI, temp;
+    
+    mult(A, A, 3, A2);
+    scalar(A, eq[0], 3, xA);
+    scalar(identity, eq[1], 3, qI);
+
+    sum(A2, xA, 3, temp);
+    sum(temp, qI, 3, R);
+
+    float d = eq[2];
+
+    scalar(R, 1/d, 3, R);
+}
+
+void cayley_hamilton(mat A, float eq[3]){
+    float trace = A[0][0] + A[1][1] + A[2][2];
+    float determinant = det(A);
+
     int min[3][2] = {
         {1,2},
         {0,2},
         {0,1},
     };
-    int q = 0;
-    for (i = 0; i < 3;i++){
-        q += mat2[min[i][0]][min[i][0]]*mat2[min[i][1]][min[i][1]] - mat2[min[i][0]][min[i][1]]*mat2[min[i][1]][min[i][0]];
+    float q = 0;
+    for (int i = 0; i < 3;i++){
+        q += A[min[i][0]][min[i][0]]*A[min[i][1]][min[i][1]] - A[min[i][0]][min[i][1]]*A[min[i][1]][min[i][0]];
     }
+    eq[0] = -trace;
+    eq[1] = q;
+    eq[2] = determinant;
 
-    printf("λ³- (%d)λ² + (%d)λ - (%d) = 0\n",x,q,det1);
+    printf("Characteristic eq: λ³ - (%0.2f)λ² + (%0.2f)λ - (%0.2f) = 0\n",
+           trace, q, determinant);
 }
 
 
 int main() {
-    int matrix1[3][3] = {
-        {6,6,3},
-        {1,2,3},
-        {1,3,4}
-    };
-    
-    int matrix2[3][3] = {
-        {2,1,1},
+
+    mat B = {
+        {1,0,1},
         {0,1,0},
-        {1,1,2}
+        {-1,0,1}
     };
 
-    int matrix3[3][3] = {
-        {20,16,17},
-        {45,-41,80},
-        {51,54,32}
+    mat diagonal = {
+        {1,0,0},
+        {0,2,0},
+        {0,0,3}
     };
-    
-    
-    // print(matrix1,3);
-    // print(matrix2,3);
-    int result1[3][3];
-    int result2[3][3];
-    cayley_hamilton(matrix2);
-    cayley_hamilton(matrix3);
+
+    mat B_1; // B-1
+    mat A;mat PD;
+
+    float eq[3];
+    cayley_hamilton(B, eq);
+    printf("\n");
+    inverse(B, eq, 3, B_1);
+    mult(B,diagonal,3,PD);
+    mult(PD,B_1,3,A);
+    print(A,3);
+
     return 0;
 }
